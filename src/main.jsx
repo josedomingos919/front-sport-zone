90ce7d892ca9f74bf22b5241749f07197d26fab4
josedomingos919 <josedomingos919@gmail.com>
2026-01-26 01:57:55 +0100

    feat: remove redux store

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { HelmetProvider } from 'react-helmet-async'
-import { Provider } from 'react-redux'
-import App from '@/App.jsx'
-import { store } from '@/app/store'
-import '@/styles/main.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { HelmetProvider } from "react-helmet-async";
+import App from "@/App.jsx";
+import "@/styles/main.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <HelmetProvider>
-      <Provider store={store}>
-        <App />
-      </Provider>
+      <App />
     </HelmetProvider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
