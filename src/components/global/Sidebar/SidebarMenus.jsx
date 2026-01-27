4f71cda55b79e0516c4911855c0797c4a652343d
josedomingos919 <josedomingos919@gmail.com>
2026-01-27 10:50:30 +0100

    feat: add atletas

--- src/components/global/Sidebar/SidebarMenus.jsx
+++ src/components/global/Sidebar/SidebarMenus.jsx
@@ -4,6 +4,9 @@ import { FaCommentsDollar } from "react-icons/fa";
 import { ImFlag } from "react-icons/im";
 import { RiUserSettingsFill } from "react-icons/ri";
 import { PiCertificateFill } from "react-icons/pi";
+import { FaPeopleGroup } from "react-icons/fa6";
+import { FaCalendarAlt } from "react-icons/fa";
+import { ImProfile } from "react-icons/im";
 
 export const sidebarmenus = [
   {
@@ -30,5 +33,20 @@ export const sidebarmenus = [
     title: "Equipa",
     icon: <ImFlag />,
     to: "/equipa",
+  },
+  {
+    title: "Atletas",
+    icon: <FaPeopleGroup />,
+    to: "/atletas",
+  },
+  {
+    title: "Calendário",
+    icon: <FaCalendarAlt />,
+    to: "/calendario",
+  },
+  {
+    title: "Scout Zone",
+    icon: <ImProfile />,
+    to: "/scout-zone",
   },
 ];
